8e39872f613747a37e34593fe29dae165bd50df5
Nacho Cortizo <nachocpol@gmail.com>
2020-03-08 16:47:54 +0000

    Script to build main solution.

---
+++ BuildAwesomeSolution.bat
@@ -0,0 +1,8 @@
+echo "====== AwesomeSolution ======"
+
+MSBuild.exe "AwesomeSolution.sln" /target:Build /p:Configuration=Debug /p:Platform=x64 /p:WindowsTargetPlatformVersion=10.0.16299.0 /p:PlatformToolset=v141 /nologo
+
+MSBuild.exe "AwesomeSolution.sln" /target:Build /p:Configuration=Release /p:Platform=x64 /p:WindowsTargetPlatformVersion=10.0.16299.0 /p:PlatformToolset=v141 /nologo
+
+
+pause
