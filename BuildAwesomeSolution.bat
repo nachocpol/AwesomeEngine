ada7eab8c5f2794fd50054aef27a790e321feda3
Nacho Cortizo <nachocpol@gmail.com>
2020-03-08 16:50:35 +0000

    Fixes to build scripts.

--- BuildAwesomeSolution.bat
+++ BuildAwesomeSolution.bat
@@ -1,3 +1,5 @@
+call GenSolution.bat
+
 echo "====== AwesomeSolution ======"
 
 MSBuild.exe "AwesomeSolution.sln" /target:Build /p:Configuration=Debug /p:Platform=x64 /p:WindowsTargetPlatformVersion=10.0.16299.0 /p:PlatformToolset=v141 /nologo
--- GenSolution.bat
+++ GenSolution.bat
@@ -1,2 +1,1 @@
 premake5.exe vs2017
-pause
