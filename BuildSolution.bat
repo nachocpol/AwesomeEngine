162ed5b1d0b16b117d6a5fd480d8937bca92db55
nacho <nachocpol@gmail.com>
2025-10-08 21:05:07 +0200

    Going to move the projects to a separate repo. Added again the bat to build the solution, this will come in handy later on.

---
+++ BuildSolution.bat
@@ -0,0 +1,9 @@
+call GenSolution.bat
+
+echo "====== AwesomeSolution ======"
+
+MSBuild.exe "AwesomeSolution.sln" /target:Build /p:Configuration=Debug /p:Platform=x64 /nologo
+
+MSBuild.exe "AwesomeSolution.sln" /target:Build /p:Configuration=Release /p:Platform=x64 /nologo
+
+pause
