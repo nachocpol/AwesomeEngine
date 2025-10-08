5f01a88562140c2b7c1c86897667cfbc1afa827c
nacho <nachocpol@gmail.com>
2025-10-08 21:10:53 +0200

    Added mt toggle

--- BuildSolution.bat
+++ BuildSolution.bat
@@ -2,8 +2,8 @@ call GenSolution.bat
 
 echo "====== AwesomeSolution ======"
 
-MSBuild.exe "AwesomeSolution.sln" /target:Build /p:Configuration=Debug /p:Platform=x64 /nologo
+MSBuild.exe "AwesomeSolution.sln" /target:Build /p:Configuration=Debug /p:Platform=x64 /nologo -m
 
-MSBuild.exe "AwesomeSolution.sln" /target:Build /p:Configuration=Release /p:Platform=x64 /nologo
+MSBuild.exe "AwesomeSolution.sln" /target:Build /p:Configuration=Release /p:Platform=x64 /nologo -m
 
 pause
